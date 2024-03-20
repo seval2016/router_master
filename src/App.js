2a38d8d38188049df78e828d111240be936b2818
seval2016 <sevalsenturk2016@gmail.com>
2024-03-20 21:50:47 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
